8175d47837cc883187f2733878e5a6ee561756fe
Fabs90 <fabianjuliansyah89@gmail.com>
2022-07-20 21:21:42 +0700

    Menambahkan file index php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cobain fetch ygy</title>
+</head>
+<body>
+    <h1>Login Form</h1>
+    <form action="" method="post">
+    Name: <input type="text" name="name">
+    E-mail: <input type="text" name="email">
+    Website: <input type="text" name="website">
+    Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+    Gender:
+    <input type="radio" name="gender" value="female">Female
+    <input type="radio" name="gender" value="male">Male
+    <input type="radio" name="gender" value="other">Other
+    </form>
+</body>
+</html>
