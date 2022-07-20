b23565f389e83856bdac56f6fa1788f93f04f891
Fabs90 <fabianjuliansyah89@gmail.com>
2022-07-20 21:33:39 +0700

    Menambahkan file css

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cobain fetch ygy</title>
 </head>
+<link rel="stylesheet" href="style.css">
 <body>
     <h1>Login Form</h1>
     <form action="" method="post">
